ff8b3ec711a95c0df9e26bc0fea08dfebe7a6de5
mefa <mefaba@outlook.com>
2021-06-30 15:48:18 -0400

    Update naming of user id field

--- server/models/Meeting.js
+++ server/models/Meeting.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
 
 const meetingSchema = new mongoose.Schema({
-  user_id: {
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'user' 
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
   },
   duration: {
     type: Number,
-  }
+  },
 });
-
 
 module.exports = Meeting = mongoose.model("meeting", meetingSchema);
