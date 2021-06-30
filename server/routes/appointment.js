f6759b028346c9983609a073d8351238228944b2
Dean Pierrot <dapierrot21@gmail.com>
2021-06-30 16:12:50 -0400

    Corrected route issue.

--- server/routes/appointment.js
+++ server/routes/appointment.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const protect = require("../middleware/auth");
 const { createAppointment, getAllAppointments } = require("../controllers/appointment");
 
-router.route("/appointment").post(createAppointment);
+router.route("/").post(createAppointment);
 
-router.route("/appointments/:user_id").get(getAllAppointments);
+router.route("/user_id").get(getAllAppointments);
 
 module.exports = router;
