82f2748c6fda6fbfb32be3519cf3e934b2b2d46b
Dean Pierrot <dapierrot21@gmail.com>
2021-07-02 10:39:11 -0400

    !!!updateUser updates the wrong user!!!

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -44,5 +44,16 @@ exports.getUserByUsername = asyncHandler(async (req, res, next) => {
 // @desc Update user
 // @access Private
 exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ user: req.params.user });
 
+  if(!user) {
+    return res.status(400).json({ msg: 'User not found' });
+  }
+
+  user.username = req.body.username;
+  user.email = req.body.email;
+
+  await user.save();
+
+  res.status(200).json({ user });
 });
