032767898c758f07e5171c7cac3498e273f129c6
mefa <mefaba@outlook.com>
2021-07-02 15:15:19 -0400

    Update meeting controllers

--- server/controllers/meeting.js
+++ server/controllers/meeting.js
@@ -4,17 +4,19 @@ const asyncHandler = require("express-async-handler");
 
 exports.createMeeting = asyncHandler(async (req, res, next) => {
   const { username, duration } = req.body;
-  const loggedInUser = await User.findOne({ username }).then((doc) => {
-    const meeting = Meeting.create({
-      user_id: doc._id,
-      duration: duration,
-    });
+  const loggedInUser = await User.findOne({ username });
+  Meeting.create({
+    userId: loggedInUser._id,
+    duration: duration,
   });
   res.status(200).json({ username, duration });
 });
 
 exports.getMeetings = asyncHandler(async (req, res, next) => {
-  Meeting.find({}).then((data) => {
-    res.json(data);
+  const { username } = req.body;
+  const loggedInUser = await User.findOne({ username });
+
+  Meeting.find({ userId: loggedInUser._id }).then((meetings) => {
+    res.json(meetings);
   });
 });
