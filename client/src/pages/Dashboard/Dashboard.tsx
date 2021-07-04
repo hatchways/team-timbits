33247f13ab1b7c3043c60d4d6a833eb4ebe129f5
Dean Pierrot <dapierrot21@gmail.com>
2021-07-04 02:39:54 -0400

    changed grid item to grif container

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,5 @@
 import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import useStyles from './useStyles';
@@ -29,12 +30,12 @@ export default function Dashboard(): JSX.Element {
   }
 
   return (
-    <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
+    <Container maxWidth={'lg'} component="main" className={`${classes.root} ${classes.dashboard}`}>
       <CssBaseline />
-      <Grid item>
+      <Grid container>
         {/* <ChatSideBanner loggedInUser={loggedInUser} /> */}
         <NavBar loggedInUser={loggedInUser} />
       </Grid>
-    </Grid>
+    </Container>
   );
 }
