3d6b1f173e95d6dcb0676b39e1cc24190d733f0f
Dean Pierrot <dapierrot21@gmail.com>
2021-07-04 02:37:35 -0400

    Updated navbar

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import useStyles from './useStyles';
 import AvatarDisplay from '../AvatarDisplay/AvatarDisplay';
 import { User } from '../../interface/User';
-import { Link } from '@material-ui/core';
-import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import AuthMenu from '../AuthMenu/AuthMenu';
 
@@ -13,17 +14,29 @@ interface Props {
 }
 
 const NavBar = ({ loggedInUser }: Props): JSX.Element => {
+  const classes = useStyles();
   return (
-    <Grid>
-      <Box>
+    <AppBar className={classes.root} position="static">
+      <Toolbar>
+        <Typography className={classes.title} variant="h6">
+          MyCalendarApp
+        </Typography>
+        <Typography>
+          <Link color="textPrimary" variant="h6">
+            Home
+          </Link>
+          <Link color="textPrimary" variant="h6">
+            Intergation
+          </Link>
+          <Link color="textPrimary" variant="h6">
+            Upgrade
+          </Link>
+        </Typography>
         <AvatarDisplay loggedIn user={loggedInUser} />
         <Typography variant="h5">{loggedInUser.username}</Typography>
         <AuthMenu />
-        <Link>Home</Link>
-        <Link>Intergation</Link>
-        <Link>Upgrade</Link>
-      </Box>
-    </Grid>
+      </Toolbar>
+    </AppBar>
   );
 };
 export default NavBar;
