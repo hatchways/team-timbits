39069b98e9725182523374ef8c4e4017565f03ec
Dean Pierrot <dapierrot21@gmail.com>
2021-07-04 19:03:43 -0400

    CSS Styling

---
+++ client/src/components/EventType/useStyles.ts
@@ -0,0 +1,14 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    marginTop: '13px',
+    marginLeft: '15px',
+  },
+});
+
+export default useStyles;
