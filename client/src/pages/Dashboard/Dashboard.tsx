e6fea87c471c25c556e6643f7f5c4df77e9384e3
Dean Pierrot <dapierrot21@gmail.com>
2021-07-03 20:23:34 -0400

    added Grid item for navbar.

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -5,7 +5,8 @@ import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
+//import ChatSideBanner from '../../components/ChatSideBanner/ChatSideBanner';
+import NavBar from '../../components/NavBar/NavBar';
 import { useEffect } from 'react';
 
 export default function Dashboard(): JSX.Element {
@@ -30,8 +31,9 @@ export default function Dashboard(): JSX.Element {
   return (
     <Grid container component="main" className={`${classes.root} ${classes.dashboard}`}>
       <CssBaseline />
-      <Grid item className={classes.drawerWrapper}>
-        <ChatSideBanner loggedInUser={loggedInUser} />
+      <Grid item>
+        {/* <ChatSideBanner loggedInUser={loggedInUser} /> */}
+        <NavBar loggedInUser={loggedInUser} />
       </Grid>
     </Grid>
   );
