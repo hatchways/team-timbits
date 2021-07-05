c8be8dd672b3617d739933105e893913b6127459
mefa <mefaba@outlook.com>
2021-07-05 13:03:12 -0400

    Update User Model for oauth compatibility

--- server/models/User.js
+++ server/models/User.js
@@ -4,22 +4,26 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
-    required: true,
-    unique: true
+    unique: true,
   },
   email: {
     type: String,
-    required: true,
-    unique: true
+  },
+  gmail: {
+    type: String,
+    unique: true,
+  },
+  googleId: {
+    type: String,
+    unique: true,
   },
   password: {
     type: String,
-    required: true
   },
   register_date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
