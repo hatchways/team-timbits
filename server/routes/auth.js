4c8f129ee49df4e5771839418edcb9ba29c4bd20
mefa <mefaba@outlook.com>
2021-07-05 13:11:32 -0400

    update oauth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { validateRegister, validateLogin } = require('../validate');
-const protect = require('../middleware/auth');
-const {
-  registerUser,
-  loginUser,
-  loadUser,
-  logoutUser,
-} = require('../controllers/auth');
+const { validateRegister, validateLogin } = require("../validate");
+const passport = require("passport");
+const { protect, passportProtect } = require("../middleware/auth");
+const { registerUser, loginUser, loadUser, logoutUser, checkUserEmail } = require("../controllers/auth");
 
-router.route('/register').post(validateRegister, registerUser);
+router.route("/register").post(validateRegister, registerUser);
 
-router.route('/login').post(validateLogin, loginUser);
+router.route("/login").post(validateLogin, loginUser);
 
-router.route('/user').get(protect, loadUser);
+router.route("/user").get(passportProtect, loadUser);
 
-router.route('/logout').get(logoutUser);
+router.route("/logout").get(logoutUser);
+
+router.route("/email/:email").post(checkUserEmail);
+
+router
+  .route("/google")
+  .get(passport.authenticate("google", { scope: ["profile", "email", "https://www.googleapis.com/auth/calendar"] }));
+
+router.route("/google/callback").get(passport.authenticate("google", { failureRedirect: "/failure" }), (req, res) => {
+  res.redirect("http://localhost:3000/dashboard");
+});
 
 module.exports = router;
