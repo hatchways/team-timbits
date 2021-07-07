55af0c1a287ee8f9578bdae6c4018b26b28d08f4
mefa <mefaba@outlook.com>
2021-07-07 14:49:01 -0400

    setup create-new-event-backend

--- server/controllers/meeting.js
+++ server/controllers/meeting.js
@@ -3,13 +3,21 @@ const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 
 exports.createMeeting = asyncHandler(async (req, res, next) => {
-  const { username, duration } = req.body;
-  const loggedInUser = await User.findOne({ username });
-  Meeting.create({
-    userId: loggedInUser._id,
-    duration: duration,
+  const { name, description, duration, url } = req.body;
+  //const id = req.session.userId
+  const id = "60df3ce18133445ba87a4fee";
+  const generatedUrl = `/event/${id}/${url}`;
+  const meetingCreated = await Meeting.create({
+    userId: id,
+    name,
+    description,
+    duration,
+    url: generatedUrl,
   });
-  res.status(200).json({ username, duration });
+  if (meetingCreated) {
+    res.status(200).json({ eventId: meetingCreated._id });
+  }
+  //TODO handle error
 });
 
 exports.getMeetings = asyncHandler(async (req, res, next) => {
--- server/routes/meeting.js
+++ server/routes/meeting.js
@@ -5,6 +5,7 @@ const { createMeeting, getMeetings } = require("../controllers/meeting");
 
 router.route("/").get(getMeetings);
 
+//TODO protect this route with passportProtect
 router.route("/create").post(createMeeting);
 
 module.exports = router;
