5665b4d47840329000045aa71a392adad3194536
Dean Pierrot <dapierrot21@gmail.com>
2021-07-08 06:59:39 -0400

    Once again more type like errors.

--- client/src/components/Onboarding/ProfileSetting/ProfileSetting.tsx
+++ client/src/components/Onboarding/ProfileSetting/ProfileSetting.tsx
@@ -1,48 +1,69 @@
-import { TextField, Divider, Typography, MenuItem, Grid } from '@material-ui/core';
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { TextField, InputAdornment } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types';
 import moment from 'moment-timezone';
-import { Component } from 'react';
-//import useStyles from './useStyles';
 
-export class ProfileSetting extends Component {
-  continue(e?: any) {
-    this.props.nextStep();
-  }
-  props: any;
-  setState: any;
-  render() {
-    const { values, handleChange } = this.props;
-    const names = moment.tz.names();
-    const { zoneName } = this.setState('EST');
-
-    return (
-      <React.Fragment>
-        <Grid container>
-          <TextField name="url" label="URL" onChange={handleChange('url')} defaultValue={values.url} />
-          <Divider orientation="vertical" />
-          <Typography variant="h6"></Typography>
-          <Typography variant="h6">Select your timezone:</Typography>
-          <TextField
-            id="standard-select-currency"
-            name="timezone"
-            select
-            label="Select"
-            value={zoneName}
-            onChange={handleChange('timezone')}
-            helperText="Please select your utc time"
-          >
-            {names.map((option) => (
-              <MenuItem key={option} value={option}>
-                <Typography component={'div'} variant="h5">
-                  UTC Time: {option} ({moment.tz(option).format('HH:mm')})
-                </Typography>
-              </MenuItem>
-            ))}
-          </TextField>
-        </Grid>
-      </React.Fragment>
-    );
-  }
+interface Props {
+  url: string,
+  setUrl: string,
+  setTimeZone: string,
+  btnText: string,
+  handleProfileSettingSubmit: any,
 }
 
-export default ProfileSetting;
+function ProfileSetting({ url, setUrl, setTimeZone, btnText, handleProfileSettingSubmit }: Props) {
+  return (
+    <Grid container spacing={4} alignItems="center">
+      <Grid item xs={4}>
+        <div>Create Your CalendApp URL: </div>
+      </Grid>
+
+      <Grid item xs={8}>
+        <TextField
+          id="url-field"
+          variant="outlined"
+          value={url}
+          //onChange={}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start" component="div">
+                calendapp.com/
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+
+      <Grid item xs={4}>
+        <div>Select your time zone: </div>
+      </Grid>
+
+      <Grid item xs={8}>
+        <Autocomplete
+          id="timezone-field"
+          options={moment.tz.names()}
+          getOptionLabel={(option) => option + ' (EST' + moment.tz(option).format('Z') + ')'}
+          renderInput={(params) => <TextField {...params} label="Type a city or zone" variant="outlined" />}
+          onChange={(e, v) => (v)}
+        />
+      </Grid>
+      <div>
+        {/* <OnboardingButton  progressText={btnText} submitForm={handleProfileSettingSubmit} /> */}
+      </div>
+    </Grid>
+  );
+}
+
+ProfileSetting.propTypes = {
+  classes: PropTypes.object.isRequired,
+  url: PropTypes.string.isRequired,
+  setUrl: PropTypes.func.isRequired,
+  setTimeZone: PropTypes.func.isRequired,
+  btnText: PropTypes.string.isRequired,
+  handleProfileSubmit: PropTypes.func.isRequired,
+};
+
+export default (ProfileSetting);
