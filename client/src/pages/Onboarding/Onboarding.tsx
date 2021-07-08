b93fc107f4a21267672a49651b9f617ac73d6032
Dean Pierrot <dapierrot21@gmail.com>
2021-07-08 07:01:03 -0400

    Type errors

--- client/src/pages/Onboarding/Onboarding.tsx
+++ client/src/pages/Onboarding/Onboarding.tsx
@@ -1,48 +1,149 @@
-import React, { Component } from 'react';
-import ProfileSetting from '../../components/Onboarding/ProfileSetting/ProfileSetting';
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import PropTypes from 'prop-types';
+import handleFetchErrors from '../../helpers/utils';
+import { Paper, Divider } from "@material-ui/core";
+import ProfileSetting from "../../components/Onboarding/ProfileSetting/ProfileSetting";
+import ProgressBar from "../../components/Onboarding/ProgressBar";
+import Availability from "../../components/Onboarding/Availability/Availability";
 
-export class Onboarding extends Component {
-  state = {
-    step: 1,
-    url: String,
-    timezone: String,
-  };
+const progressText = {
+  profileSetting: {
+    header: 'Welcome to CalendApp',
+    btnText: 'Continue',
+  },
+  confirm: {
+    header: 'Your Google Calendar is connected',
+    btnText: 'Continue',
+  },
+  availability: {
+    header: 'Set your availability',
+    btnText: 'Done'
+  }
+};
+interface Props {
+  url: string,
+  timezone: string,
+  hours: string,
+  days: string,
+  type: string,
+  activeStep: any,
+}
 
-  //Proceed to next step
-  nextStep = () => {
-    const { step } = this.state;
-    this.setState({
-      step: step + 1,
-    });
-  };
+function Onboarding({ type, activeStep }: Props) {
+  const [url, setURL] = useState('');
+  const [timezone, setTimeZone] = useState('');
+  const [hours, setHours] = useState({ start: '6:00', end: '21:00' })
+  const [days, setDays] = useState({
+    Sunday: false,
+    Monday: true,
+    Tuesday: true,
+    Wednesday: true,
+    Thursday: true,
+    Friday: true,
+    Saturday: false,
+  });
+}
 
-  //Previous step.
-  prevStep = () => {
-    const { step } = this.state;
-    this.setState({
-      step: step - 1,
-    });
-  };
+let history = useHistory();
 
-  //Handle fields change
-  handleChange = (input: any) => (e: { target: { value: any } }) => {
-    this.setState({ [input]: e.target.value });
-  };
+function getSteps(this: any, type: string) {
+  if (type === 'profileSetting') {
+    return (
+      <ProfileSetting
+        handleProfileSettingSubmit={handleProfileSettingSubmit}
+        btnText={progressText[type].btnText}
+        url={this.url}
+        timezone={this.timezone}
+        setUrl={this.setUrl}
+        setTimeZone={this.setTimeZone}
+      />
+    );
+  }
+  if (type === 'confirm') {
 
-  render() {
-    const { step } = this.state;
-    const { url, timezone } = this.state;
-    const values = { url, timezone };
-
-    switch (step) {
-      case 1:
-        return <ProfileSetting nextStep={this.nextStep} handleChange={this.handleChange} values={values} />;
-      case 2:
-        return <h1>Confirm</h1>;
-      case 3:
-        return <h1>Availability</h1>;
-    }
+  }
+  if (type === 'availability') {
+    return (
+      <Availability
+        submitForm={submitForm}
+        hours={this.hours}
+        setHours={this.setHours}
+        days={this.days}
+        setDays={this.setDays} 
+      />
+    );
   }
 }
 
-export default Onboarding;
+const handleConfirmSubmit = () => {
+  history.push('/availability');
+};
+
+const handleProfileSettingSubmit = () => {
+  if (url === '' || timezone === '') {
+    return;
+  }
+  fetch(`/api/user/url?=${url}`) 
+  .then(handleFetchErrors)
+  .then((res) => res.json())
+  .then((data) => {
+    if (!data.isUnique) {
+      return;
+    }
+    history.push('/confirm');
+  })
+  .catch((err) => {
+    console.error(err);
+  })
+}
+
+const submitForm = () => {
+  const profileInfo = {
+    url,
+    timeZone,
+    hours,
+    days,
+  };
+  const sub = // CREATE A auth file;
+
+  fetch(`/api/user/profile/${sub}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(profileInfo),
+  })
+    .then(handleFetchErrors)
+    .then((res) => {
+      if (res.status !== 200) return;
+      history.push('/dashboard');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+
+    return (
+      <Paper elevation={6} className={classes.paper}>
+        <div className={classes.headRow}>
+          <div className={classes.headContent}>
+            <h2>{progressText.header}</h2>
+            <ProgressBar activeStep={activeStep} />
+          </div>
+          <Divider className={classes.divider} />
+        </div>
+        {getSteps(type)}
+      </Paper>
+    );
+    
+};
+
+
+Onboarding.propTypes = {
+  classes: PropTypes.object.isRequired,
+  type: PropTypes.string.isRequired,
+  activeStep: PropTypes.number.isRequired,
+};
+
+export default (Onboarding);
