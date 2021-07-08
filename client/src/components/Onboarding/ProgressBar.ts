ada43a5dfe48bb99cf31e379e1b024558d00aa7c
Dean Pierrot <dapierrot21@gmail.com>
2021-07-08 06:56:09 -0400

    ProgressBar but return type for progess bar isnt liked.

---
+++ client/src/components/Onboarding/ProgressBar.ts
@@ -0,0 +1,28 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
+const styles = (theme: any) => ({
+  root: {
+    flex: '0 0 40%',
+    alignSelf: 'center',
+    height: '0.5rem',
+    borderRadius: '5px',
+  },
+  colorPrimary: {
+    backgroundColor: '#ccc',
+  },
+  barColorPrimary: {
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: '5px',
+  },
+});
+
+function ProgressBar(props: any) {
+  const { activeStep } = props;
+
+
+};
+
+
+export default (ProgressBar);
