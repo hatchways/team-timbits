959a05de5149699d5a5c3cbb943d7a8be39e7780
Dean Pierrot <dapierrot21@gmail.com>
2021-07-09 10:20:35 -0400

    Checks for user url

---
+++ client/src/helpers/APICalls/checkUserUrl.ts
@@ -0,0 +1,17 @@
+import { AuthApiData } from '../../interface/AuthApiData';
+import { FetchOptions } from '../../interface/FetchOptions';
+
+const checkUserUrl = async (url: string): Promise<AuthApiData> => {
+  const fetchOptions: FetchOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
+  };
+  return await fetch(`/api/user/uniqueUrl?url=${url}`, fetchOptions)
+    .then((res) => res.json())
+    .catch(() => ({
+      error: { message: 'Error: Url is not found.' },
+    }));
+};
+
+export default checkUserUrl;
