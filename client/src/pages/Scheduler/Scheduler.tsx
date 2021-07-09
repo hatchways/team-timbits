f93490806a59c2e7cff21f519f701ed827f48802
hamaad102 <hamaad102@gmail.com>
2021-07-09 05:22:59 -0400

    Added calendar as well as time block.

--- client/src/pages/Scheduler/Scheduler.tsx
+++ client/src/pages/Scheduler/Scheduler.tsx
@@ -1,17 +1,38 @@
+import axios from 'axios';
+import { useState } from 'react';
+import Calendar from 'react-calendar';
 import { useParams } from 'react-router-dom';
 
 // Material-UI and Style
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
+import Grid, { GridSize } from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import useStyles from './useStyles';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+
 import Typography from '@material-ui/core/Typography';
+import Zoom from '@material-ui/core/Zoom';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+
+// Calendar Style
+import 'react-calendar/dist/Calendar.css';
+
+// Components
+import Time from '../../components/Time/Time';
 
 function Scheduler(): JSX.Element {
+  // Local State
+  const [size, setSize] = useState<GridSize>(6);
+  const [open, setOpen] = useState<boolean>(false);
+  const [checked, setChecked] = useState<boolean>(false);
+  const [selectedDay, setSelectedDay] = useState<Date>(new Date());
+
+  // Material-UI & Style
   const theme = useTheme();
   const classes = useStyles();
   const matches = useMediaQuery(theme.breakpoints.up('lg'));
@@ -24,32 +45,52 @@ function Scheduler(): JSX.Element {
     <>
       <CssBaseline />
       <Container className={classes.root}>
-        <Grid container className={classes.pageHeight} alignItems="center">
-          <Grid item xs={12}>
-            <Paper elevation={3}>
-              <Grid container>
-                <Grid item lg={6} xs={12} className={matches ? classes.right : classes.bottom}>
-                  <Container>
-                    <Typography variant="h6">{username}</Typography>
-                    <Typography variant="h4">{withoutLetters} minute meeting</Typography>
-                    <Grid container spacing={1} alignItems="flex-end">
-                      <Grid item>
-                        <AccessTimeOutlinedIcon fontSize="medium" />
-                      </Grid>
-                      <Grid item>
-                        <Typography variant="h6"> {withoutLetters} min</Typography>
-                      </Grid>
-                    </Grid>
-                  </Container>
-                </Grid>
-                <Grid item lg={6} xs={12}>
-                  <Container>
-                    <Typography variant="h4">Select a Date & Time</Typography>
-                  </Container>
+        <Grid container alignItems="center" justifyContent="center" className={classes.root}>
+          <Paper elevation={16} className={checked ? classes.paperExpand : classes.paper}>
+            <Grid container className={classes.height}>
+              <Grid
+                item
+                lg={size}
+                xs={12}
+                className={`${matches ? classes.right : classes.bottom} ${classes.gridItem}`}
+              >
+                <Typography variant="h6">{username}</Typography>
+                <Typography variant="h4">{withoutLetters} minute meeting</Typography>
+                <Grid container spacing={1} alignItems="flex-end">
+                  <Grid item>
+                    <AccessTimeOutlinedIcon fontSize="medium" />
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="h6"> {withoutLetters} min</Typography>
+                  </Grid>
                 </Grid>
               </Grid>
-            </Paper>
-          </Grid>
+
+              <Grid item lg={size} xs={6} className={`${classes.gridItem}`}>
+                <Typography variant="h4">Select a Date & Time</Typography>
+                <Calendar
+                  value={selectedDay}
+                  minDate={new Date()}
+                  onClickDay={(value, event) => {
+                    setSelectedDay(value);
+                    setSize(4);
+                    setChecked(true);
+                  }}
+                />
+              </Grid>
+
+              <Zoom in={checked} style={{ transitionDelay: checked ? '1s' : '0ms' }}>
+                <Grid item lg={size} xs={6}>
+                  <Typography>{selectedDay.toString().slice(0, 10).replace(/-/g, '')}</Typography>
+                  {/* Map out the times available from what the user has set */}
+                  <Button>
+                    <FiberManualRecordIcon color="primary" fontSize="small" />
+                    9:45
+                  </Button>
+                </Grid>
+              </Zoom>
+            </Grid>
+          </Paper>
         </Grid>
       </Container>
     </>
--- client/src/pages/Scheduler/useStyles.ts
+++ client/src/pages/Scheduler/useStyles.ts
@@ -2,14 +2,24 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(() => ({
   root: {
-    minHeight: '75vh',
+    height: '58.406rem',
   },
-  pageHeight: {
-    minHeight: 'inherit',
-    justifyContent: 'center',
+  paper: {
+    width: 'calc(100% / 1.3)',
+    transition: 'width 1s',
+  },
+  paperExpand: {
+    width: 'calc(100% / 0.8)!important',
+    transition: 'width 1s',
+  },
+  height: {
+    height: '100%',
+  },
+  gridItem: {
+    transition: 'all 1s',
   },
   right: {
-    borderRight: '1px solid hsl(0, 0%, 75%)',
+    borderRight: '1px solid hsl(0, 0%, 80%)',
   },
   bottom: {
     borderBottom: '1px solid hsl(0, 0%, 75%)',
