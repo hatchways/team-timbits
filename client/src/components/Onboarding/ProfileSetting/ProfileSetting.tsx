abc176730ce3f98164096cb10a08c55abce91ab5
Dean Pierrot <dapierrot21@gmail.com>
2021-07-09 10:19:47 -0400

    Implemented ProfileSetting page

--- client/src/components/Onboarding/ProfileSetting/ProfileSetting.tsx
+++ client/src/components/Onboarding/ProfileSetting/ProfileSetting.tsx
@@ -1,68 +1,122 @@
-
-import { TextField, InputAdornment } from '@material-ui/core';
+import { TextField, InputAdornment, CircularProgress } from '@material-ui/core';
 import { Autocomplete } from '@material-ui/lab';
 import Grid from '@material-ui/core/Grid';
-import PropTypes from 'prop-types';
 import moment from 'moment-timezone';
-import CustomButton from '../OnboardingButton';
+import { Formik, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import { Typography } from '@material-ui/core';
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useAuth } from '../../../context/useAuthContext';
+import { useSocket } from '../../../context/useSocketContext';
 
 interface Props {
-  url: string,
-  setUrl: any,
-  setTimeZone: any,
-  btnText: string,
-  handleProfileSettingSubmit: any,
+  handleSubmit: (
+    {
+      url,
+      timezone,
+    }: {
+      url: string;
+      timezone: any;
+    },
+    {
+      setStatus,
+      setSubmitting,
+    }: FormikHelpers<{
+      url: string;
+      timezone: string;
+    }>,
+  ) => void;
 }
 
-function ProfileSetting({ url, setUrl, setTimeZone, btnText, handleProfileSettingSubmit }: Props) {
+const ProfileSetting = ({ handleSubmit }: Props): JSX.Element => {
+  const { loggedInUser } = useAuth();
+  const { initSocket } = useSocket();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    initSocket();
+  }, [initSocket]);
+
+  if (loggedInUser === undefined) return <CircularProgress />;
+  if (!loggedInUser) {
+    history.push('/login');
+    return <CircularProgress />;
+  }
+
   return (
-    <Grid container spacing={4} alignItems="center">
-      <Grid item xs={4}>
-        <div>Create Your CalendApp URL: </div>
-      </Grid>
-
-      <Grid item xs={8}>
-        <TextField
-          id="url-field"
-          variant="outlined"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start" component="div">
-                | userLastname
-              </InputAdornment>
-            ),
-          }}
-        />
-      </Grid>
-
-      <Grid item xs={4}>
-        <div>Select your time zone: </div>
-      </Grid>
-
-      <Grid item xs={8}>
-        <Autocomplete
-          id="timezone-field"
-          options={moment.tz.names()}
-          getOptionLabel={(option) => option + ' (EST' + moment.tz(option).format('Z') + ')'}
-          renderInput={(params) => <TextField {...params} label="Select your timezone" variant="outlined" />}
-          onChange={(e, v) => setTimeZone(v)}
-        />
-      </Grid>
-      <div>
-        <CustomButton text={btnText} submitForm={handleProfileSettingSubmit} />
-      </div>
-    </Grid>
+    <Formik
+      initialValues={{
+        url: '',
+        timezone: '',
+      }}
+      validationSchema={Yup.object().shape({
+        url: Yup.string()
+          .required('Url is required')
+          .test('oneOfAKindUrl', 'url is not valid', function (url: any) {
+            if (url === '') {
+              return;
+            }
+            fetch(`/api/user/uniqueUrl?url=${url}`)
+              .then((res) => res.json())
+              .then((data) => {
+                if (!data.isUnique) {
+                  return;
+                }
+              })
+              .catch((err) => {
+                console.error(err);
+              });
+            return url;
+          }),
+        timezone: Yup.string().required('Please choose a timezone'),
+      })}
+      onSubmit={handleSubmit}
+    >
+      {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <form onSubmit={handleSubmit} noValidate>
+          <Grid container spacing={4} alignItems="center">
+            <Grid item xs={4}>
+              <Typography variant="h6">Create Your CalendApp URL: </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="url-field"
+                name="url"
+                variant="outlined"
+                value={values.url}
+                helperText={touched.url ? errors.url : ''}
+                error={touched.url && Boolean(errors.url)}
+                onChange={handleChange}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end" component="span">
+                      | {loggedInUser.username}
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <Typography variant="h6">Choose your timezone: </Typography>
+            </Grid>
+            <Grid item xs={8}>
+              <Autocomplete
+                id="timezone-field"
+                value={values.timezone}
+                options={moment.tz.names()}
+                getOptionLabel={(option) => option + ' (' + moment.tz(option).format('ha z') + ')'}
+                getOptionSelected={(option) => values.timezone === option}
+                renderInput={(params) => <TextField {...params} label="Select your timezone" variant="outlined" />}
+                onChange={handleChange}
+              />
+            </Grid>
+          </Grid>
+        </form>
+      )}
+    </Formik>
   );
-}
-
-ProfileSetting.propTypes = {
-  url: PropTypes.string.isRequired,
-  setUrl: PropTypes.func.isRequired,
-  setTimeZone: PropTypes.func.isRequired,
-  btnText: PropTypes.string.isRequired,
-  handleProfileSubmit: PropTypes.func.isRequired,
 };
 
-export default (ProfileSetting);
+export default ProfileSetting;
