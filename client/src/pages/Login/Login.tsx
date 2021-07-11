26de51aef299405f4546e6a9ccaa5c0d17f00c5d
mefa <mefaba@outlook.com>
2021-07-11 04:03:51 -0400

    fix auth integration of other components

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -2,7 +2,6 @@ import useStyles from './useStyles';
 import { CssBaseline, Box, Grid, Paper, Typography } from '@material-ui/core';
 import { FormikHelpers } from 'formik';
 import AuthHeader from '../../components/AuthHeader/AuthHeader';
-import { useAuth } from '../../context/useAuthContext';
 import { useSnackBar } from '../../context/useSnackbarContext';
 import AppLogo from './../../components/AppLogo';
 import checkUserEmail from '../../helpers/APICalls/checkUserEmail';
@@ -12,7 +11,6 @@ import { useState } from 'react';
 
 export default function Login(): JSX.Element {
   const classes = useStyles();
-  const { updateLoginContext } = useAuth();
   const { updateSnackBarMessage } = useSnackBar();
   const [showGoogleConnect, setShowGoogleConnect] = useState(false);
 
@@ -25,7 +23,6 @@ export default function Login(): JSX.Element {
       } else if (data.success) {
         //user email exists in database, continue to login process
         setShowGoogleConnect(true);
-        updateLoginContext(data.success);
       }
     });
   };
