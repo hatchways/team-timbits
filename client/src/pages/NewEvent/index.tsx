0c8e7e448194e12203866a52488744611bca0ed5
mefa <mefaba@outlook.com>
2021-07-11 11:47:33 -0400

    create new-event-page

---
+++ client/src/pages/NewEvent/index.tsx
@@ -0,0 +1,23 @@
+// Material-UI and Style
+import { CircularProgress } from '@material-ui/core';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import NavBar from '../../components/NavBar/NavBar';
+import NewEventType from '../../components/NewEventType/NewEventType';
+import { useAuth } from '../../context/useAuthContext';
+
+export default function Dashboard(): JSX.Element {
+  const { loggedInUser } = useAuth();
+
+  if (loggedInUser === undefined) return <CircularProgress />;
+  if (!loggedInUser) {
+    return <CircularProgress />;
+  }
+  return (
+    <Container maxWidth={'lg'} component="main">
+      <CssBaseline />
+      <NavBar loggedInUser={loggedInUser} />
+      <NewEventType />
+    </Container>
+  );
+}
