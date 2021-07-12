fb1f23348cb1ee4b6e885b814cd697dd39cf0b1f
Dean Pierrot <dapierrot21@gmail.com>
2021-07-12 12:51:13 -0400

    removed routes to onboarding.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const protect = require("../middleware/auth");
-const { searchUsers, getUserByUsername, updateUser } = require("../controllers/user");
+const { searchUsers, getUserByUsername } = require("../controllers/user");
 const { route } = require("./auth");
 
 router.route("/").get(protect, searchUsers);
 router.route("/:username").get(getUserByUsername);
 router.route("/:id").post(updateUser);
+
 
 module.exports = router;
