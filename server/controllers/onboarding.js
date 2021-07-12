4f68ced8922ab95983f333b33e9e3ef4415135c3
Dean Pierrot <dapierrot21@gmail.com>
2021-07-12 12:49:53 -0400

    Onboarding controller

--- server/controllers/onboarding.js
+++ server/controllers/onboarding.js
@@ -1,18 +1,45 @@
 const User = require('../models/User');
 const asyncHandler = require('express-async-handler');
 
-// @route GET /auth/url
-// @des check for user url
-// @access Private
+// @route  POST api/user/url
+// @des    Checks for user for url and updates url with url & timezone
+// @access Public
 exports.checkUserUrl = asyncHandler(async (req, res, next) => {
-    const user = await User.findOne({ url: req.params.url });
+    const { url } = req.body;
+    const user = await User.findOne({ url: req.params.url , timezone: req.body.timezone});
 
-    if (!user) {
-        res.status(400).json({
-            error: {
-                msg: "Url not valid"
-            },
-        });
+    if (url === '' || !url.isUnique) {
+        return res.status(400).json({ msg: 'Url is not valid' });
     }
-    return res.status(200).json({ msg: 'Url checks out.' });
+     
+    const user = new User({
+      url: req.boby.url,
+    });
+
+    await user.save();
+    res.json({ user });
+
+});
+
+
+
+// @route  POST api/availability/:hours/:days
+// @des    Creates availability/
+// @access Public
+exports.createUserAvailability = asyncHandler(async (req, res) => {
+  const { hours, days } = req.body;
+  const  availability = await User.findOne({ availabilityId });
+
+  if(!hours && !days) {
+      return res.status(400).json({ msg: 'hours, days, are required fields.'});
+  }
+   
+  const availability = new Availability({
+    hours: req.body.hours,
+    days: req.body.days
+  });
+
+  await availability.save();
+  res.json({ availability });
+
 });
