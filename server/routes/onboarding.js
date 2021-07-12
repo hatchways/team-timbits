761bec46392cbc99eab967d69d5472918aa71809
Dean Pierrot <dapierrot21@gmail.com>
2021-07-12 12:50:47 -0400

    Added onboarding routes

--- server/routes/onboarding.js
+++ server/routes/onboarding.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { checkUserUrl } = require('../controllers/onboarding');
+const { updateUser } = require('../controllers/user');
+const { createUserAvailability } = require('../controllers/onboarding');
 
-router.route("/onboarding").get(checkUserUrl);
+router.route("user/:url").get(checkUserUrl);
+router.route("user/:url/:timezone").post(updateUser);
+router.route("user/availability/:hours/:days").post(createUserAvailability);
 
 module.exports = router;
