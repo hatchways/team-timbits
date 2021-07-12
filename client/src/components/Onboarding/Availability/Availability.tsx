2d4736c70c6589866342afb8a44454f22713d961
Dean Pierrot <dapierrot21@gmail.com>
2021-07-12 12:46:46 -0400

    Use formik instead but onSubmit is not happy.

--- client/src/components/Onboarding/Availability/Availability.tsx
+++ client/src/components/Onboarding/Availability/Availability.tsx
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 import { TextField, FormGroup, FormControlLabel, Checkbox } from '@material-ui/core';
+import { Formik, FormikHelpers, yupToFormErrors } from 'formik';
+import * as Yup from 'yup';
 
-export default function Availability(): JSX.Element {
+interface Props {
+  handleSubmit: (
+    {
+      hours,
+      days,
+    }: {
+      hours: string;
+      days: string;
+    },
+    {
+      setStatus,
+      setSubmitting,
+    }: FormikHelpers<{
+      hours: string;
+      days: string;
+    }>
+  ) => void;
+}
+
+const Availability = ({ handleSubmit }: Props): JSX.Element => {
   const [hours, setHours] = useState({ start: '9:00', end: '17:00' });
   const [days, setDays] = useState({
     Sunday: false,
@@ -13,21 +34,20 @@ export default function Availability(): JSX.Element {
     Saturday: false,
   });
 
-  const handleDays = (e: { target: { name: any; checked: any } }) => {
-    setDays({ ...days, [e.target.name]: e.target.checked });
+  const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+    setDays(event.target.value as any);
+    setHours(event.target.value as any);
   };
 
-  const handleHours = (e: { target: { name: any; value: any } }) => {
-    setHours({ ...hours, [e.target.name]: e.target.value });
-  };
-
-  function renderCheckBoxes(days: { [x: string]: any }) {
+  // eslint-disable-next-line
+  function renderCheckBoxes(days: any) {
     return Object.keys(days).map((day) => {
       return (
         <FormControlLabel
           id={day + '-checkbox-field'}
           key={day}
-          control={<Checkbox checked={days[day]} onChange={handleDays} name={day} />}
+          value={day}
+          control={<Checkbox checked={days[day]} onChange={handleChange} name={day} />}
           label={day + 's'}
           labelPlacement="bottom"
         />
@@ -36,34 +56,54 @@ export default function Availability(): JSX.Element {
   }
 
   return (
-    <React.Fragment>
-      <div>
+    <Formik
+     initalValues={{
+       hours: '',
+       days: '',
+     }}
+     isSubmitting={true}
+     isValidating={true}
+     validationSchema={Yup.object().shape({
+       hours: Yup.string().required('Please choose your hours.'),
+       days: Yup.object().required('Please select your days.'),
+     })}
+     onSubmit={handleSubmit}
+    >
+      {({handleSubmit, handleChange, values, touched, errors}: any) => (
+        <form onSubmit={handleSubmit}>
+          <div>
         <div>Available Hours: </div>
         <div>
           <TextField
             id="start-hours-field"
-            value={hours}
+            value={values.hours}
+            helperText={touched.hours ? errors.hours : ''}
+            error={ touched.hours && Boolean(errors.hours)}
             variant="outlined"
             type="time"
             name="start"
-            onChange={handleHours}
+            onChange={handleChange}
           />
           <span>-</span>
           <TextField
             id="end-hours-field"
-            value={hours}
+            value={values.hours}
             variant="outlined"
             type="time"
             name="end"
-            onChange={handleHours}
+            onChange={handleChange}
           />
         </div>
       </div>
 
       <div>
         <div>Available Days: </div>
-        <FormGroup row>{renderCheckBoxes(days)}</FormGroup>
+        <FormGroup row>{renderCheckBoxes(values.days)}</FormGroup>
       </div>
-    </React.Fragment>
-  );
-}
+        </form>
+      )}
+      </Formik>
+    );
+      }
+
+  export default Availability;
