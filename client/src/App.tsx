dee414fe8e5de9c8e9febf9a8e41751bdb00e947
Dean Pierrot <dapierrot21@gmail.com>
2021-07-13 15:52:47 -0400

    merge conflict error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+// Material-UI & Style
+import { MuiThemeProvider } from '@material-ui/core';
 import { theme } from './themes/theme';
+import './App.css';
+
+// Context
 import { AuthProvider } from './context/useAuthContext';
 import { SnackBarProvider } from './context/useSnackbarContext';
+
+// Components
 import Login from './pages/Login/Login';
 import Signup from './pages/SignUp/SignUp';
 import Dashboard from './pages/Dashboard/Dashboard';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import { MuiThemeProvider } from '@material-ui/core';
+import Scheduler from './pages/Scheduler/Scheduler';
+import GoogleConnect from './components/GoogleConnect/GoogleConnect';
 import Onboarding from './pages/Onboarding/Onboarding';
 
 function App(): JSX.Element {
@@ -17,8 +26,12 @@ function App(): JSX.Element {
             <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/google" component={GoogleConnect} />
               <Route exact path="/dashboard">
                 <Dashboard />
+              </Route>
+              <Route exact path="/:username/:time">
+                <Scheduler />
               </Route>
               <Route exact path="/onboarding">
                 <Onboarding />
