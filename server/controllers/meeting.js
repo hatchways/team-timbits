fbdd362167ece7983780f06a0fab54af5106f130
mefa <mefaba@outlook.com>
2021-07-15 18:19:24 -0400

    Update meeting.js

--- server/controllers/meeting.js
+++ server/controllers/meeting.js
@@ -6,28 +6,22 @@ exports.createMeeting = asyncHandler(async (req, res, next) => {
   const { name, description, duration, url } = req.body;
 
   const generatedUrl = `/event/${req.user._id}/${url}`;
-  try {
-    const meetingCreated = await Meeting.create({
-      userId: req.user._id,
-      name,
-      description,
-      duration,
-      url: generatedUrl,
-    });
-    if (meetingCreated) {
-      res.status(200).json({ eventId: meetingCreated._id });
-    }
-  } catch (err) {
-    res.status(400).send(err);
+
+  const meetingCreated = await Meeting.create({
+    userId: req.user._id,
+    name,
+    description,
+    duration,
+    url: generatedUrl,
+  });
+  if (meetingCreated) {
+    res.status(200).json({ eventId: meetingCreated._id });
   }
 });
 
 exports.getMeetings = asyncHandler(async (req, res, next) => {
-  Meeting.find({ userId: req.user._id })
-    .then((meetings) => {
-      res.status(200).json({ success: meetings });
-    })
-    .catch((error) => {
-      res.status(400).json(error);
-    });
+  const meetings = await Meeting.find({ userId: req.user._id });
+  if (meetings) {
+    res.status(200).json({ success: meetings });
+  }
 });
