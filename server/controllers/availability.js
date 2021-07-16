cd0480e093fff14adc02839c3d2344f4defb8767
Dean Pierrot <dapierrot21@gmail.com>
2021-07-16 11:35:43 -0400

    Backend is throwing an error that reads: API key is not valid. Please pass a valid API key.  Line 14?

--- server/controllers/availability.js
+++ server/controllers/availability.js
@@ -1,37 +1,32 @@
 const asyncHandler = require('express-async-handler');
 const { google } = require('googleapis');
-const User = require('../models/User');
+const Availability = require('../models/Availability');
 
+exports.checkAvailabilitiy =  asyncHandler(async(req, res, next) => {
+  const { day } = req.params.day;
+  const userOfMeeting = Meeting.findOne({userId: req.params.user});
+  const userAvailability = Availability.findOne({ availablility: userOfMeeting });
+  const lookUpDate = userAvailability[day] = day;
 
-exports.checkAvailablitiy =  asyncHandler(async(res, req, next) => {
-  const { hours } = await User.findOne({ availability: req.params.hours });
   const calendar = 'primary';
   const cal = google.calendar({
     version: 'v3',
-    auth: `${process.env.GOOGLE_CLIENT_ID}`
+    auth: `${process.env.GOOGLE_CLIENT_ID}`,
   });
 
   // Make the query
   cal.freebusy.query({
       resource: {
           // Set times to ISO strings as such
-          timeMin: hours[0].toISOString(), 
-          timeMax: hours[1].toISOString(),
+          timeMin: Date.parse(lookUpDate),
           timeZone: 'UTC',
           items: [{ id: calendar }]
       }
   }).then((res) => {
       const busy = res.data.calendars[calendar].busy;
       const errors = res.data.calendars[calendar].errors;
-      if (undefined !== errors) {
-          console.error('Check this calendar for public free busy visibility');
-      } else if (busy.length !== 0) {
-          console.log('Busy');
-      } else {
-        return cal.calendar.calendarList.get({ calendarId: 'primary'});
-    }
-  }).catch((e) => {
-    return res.statusCode(400).json(e);
-  });
+      if(errors) return res.status(400).json({ errors });
+      res.json({ busy });
+  })
 });
 
