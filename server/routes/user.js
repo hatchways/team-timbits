39937ea94a8e67a8f54ab1247724786278c56c95
Dean Pierrot <dapierrot21@gmail.com>
2021-07-19 15:25:34 -0400

    cloudinary POST route

--- server/routes/user.js
+++ server/routes/user.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const { passportProtect } = require("../middleware/auth");
 const { searchUsers, getUserByUsername, updateUser } = require("../controllers/user");
 const { route } = require("./auth");
+const { upload } = require("../utils/multer");
+const { loadImageToCloudinariy } = require("../controllers/image-uploader");
 
 router.route("/").get(passportProtect, searchUsers);
 router.route("/:username").get(getUserByUsername);
 router.route("/:id").post(updateUser);
+
+//Cloudinary
+router.route("/", upload.single("profile-image")).post(loadImageToCloudinariy);
 
 module.exports = router;
